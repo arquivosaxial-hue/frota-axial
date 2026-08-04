b62e9db8ead0e5ac214a74d629a4d50e1468af0c
arquivosaxial-hue <arquivosaxial@gmail.com>
2026-08-04 13:34:23 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,13 +5,12 @@
 
 // IMPORTANTE: ao publicar uma nova versão do app, troque o número da versão
 // abaixo (ex: v2, v3...). Isso força os celulares a baixarem a versão nova.
-const VERSION = 'frota-axial-v66';
+const VERSION = 'frota-axial-v67';
 const CACHE = VERSION;
 
 // Arquivos essenciais do app (o "casco")
 const APP_SHELL = [
   './',
-  './index.html',
   './manifest.json',
   './icon-192.png',
   './icon-512.png',
